13999937d47d13786082d0ea04defa3d26e863e7
edd-ie <ogendoeddie@gmail.com>
2023-03-13 12:42:37 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// currentUser is defined:
+const currentUser = 'Eddie';
+
+// welcomeMessage: "Welcome to Flatbook,'currentUser'":
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// excitedWelcomeMessage: "WELCOME TO FLATBOOK, 'currentUser' !"
+let  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// shortGreeting: "Welcome," + first initial of 'currentUser' variable + !
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
